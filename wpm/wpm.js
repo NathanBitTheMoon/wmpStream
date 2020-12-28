4f7d7393c0df37da2708a81b5c1dbcacd101b581
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2020-12-28 22:15:30 +1100

    Added WPM to dependencies and added class constructor for WPM API functions

---
+++ wpm/wpm.js
@@ -0,0 +1,8 @@
+class WPM {
+    constructor() {
+        // Class for getting Windows Media information (the shit that shows up when you hit the volume keys)
+        // And allowing that information to be gotten using function calls
+        
+        this.wpmLib = require("wpm");
+    }
+}
