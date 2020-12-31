b244857035e50c771599adc7786d0e7475a2c877
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2020-12-31 16:15:13 +1100

    foregot one UwU

--- wpm/wpm.js
+++
@@ -1,8 +0,0 @@
-class WPM {
-    constructor() {
-        // Class for getting Windows Media information (the shit that shows up when you hit the volume keys)
-        // And allowing that information to be gotten using function calls
-        
-        this.wpmLib = require("wpm");
-    }
-}
